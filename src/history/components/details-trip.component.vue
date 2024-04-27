6c2eb7ae38e7081bf133971f65468ee004c92e2d
sebastian123gonzalo <sebastian123gonzalo@gmail.com>
2024-04-27 00:32:57 -0500

    Update: details-trip.component.vue styles

--- src/history/components/details-trip.component.vue
+++ src/history/components/details-trip.component.vue
@@ -38,21 +38,67 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>{{ trip.nombre }}</h1>
-    <img :src="trip.imagenEmpresa" width="100" >
-    <p>FECHA DE CARGA: {{ trip.fechaCarga }}</p>
-    <p>FECHA DE DESCARGA: {{ trip.fechaDescarga }}</p>
-    <p>LUGAR DE CARGA: {{ trip.lugarCarga }}</p>
-    <p>LUGAR DE DESCARGA: {{ trip.lugarDescarga }}</p>
-    <p>CONDUCTOR: {{ trip.conductor }}</p>
-    <p>PLACA DE CARRETA:{{ trip.placaCarreta }}</p>
-    <p>PLACA DE TRACTO: {{ trip.placaTracto }}</p>
-    <p>EMPRESA: {{ trip.empresa }}</p>
-    <p>RUC: {{ trip.ruc }}</p>
+  <div class="container-general">
+    <img :src="trip.imagenEmpresa" width="250" >
+    <div class="container-info-general">
+      <h1>{{ trip.nombre }}</h1>
+      <div class="container-info">
+        <p>FECHA DE CARGA: {{ trip.fechaCarga }}</p>
+      </div>
+      <div class="container-info">
+        <p>FECHA DE DESCARGA: {{ trip.fechaDescarga }}</p>
+      </div>
+      <div class="container-info">
+        <p>LUGAR DE CARGA: {{ trip.lugarCarga }}</p>
+      </div>
+      <div class="container-info">
+        <p>LUGAR DE DESCARGA: {{ trip.lugarDescarga }}</p>
+      </div>
+      <div class="container-info">
+        <p>CONDUCTOR: {{ trip.conductor }}</p>
+      </div>
+      <div class="container-info">
+        <p>PLACA DE CARRETA:{{ trip.placaCarreta }}</p>
+      </div>
+      <div class="container-info">
+        <p>PLACA DE TRACTO: {{ trip.placaTracto }}</p>
+      </div>
+      <div class="container-info">
+        <p>EMPRESA: {{ trip.empresa }}</p>
+      </div>
+      <div class="container-info">
+        <p>RUC: {{ trip.ruc }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-/* Aquí puedes agregar tus estilos */
+.container-general {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.container-general img {
+  margin-right: 200px;
+}
+
+.container-general h1 {
+  text-align: center;
+}
+.container-info-general {
+  display: flex;
+  flex-direction: column;
+}
+.container-info {
+  background-color: #FFA500;
+  width: 721px;
+  margin-bottom: 10px;
+}
+
+.container-info p {
+  padding: 0 10px;
+}
 </style>
